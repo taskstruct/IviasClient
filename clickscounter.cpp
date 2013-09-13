c77b18191f1be1b03c630e93d0f3d8c58ae9d6cd
taskstruct <nikolay.fatal.error@gmail.com>
2013-09-13 15:53:03 +0300

    Initial implementation of Clicks Counter

--- clickscounter.cpp
+++ clickscounter.cpp
@@ -1,6 +1,57 @@
 #include "clickscounter.h"
 
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtSql/QSqlDatabase>
+#include <QtSql/QSqlQuery>
+
+extern QNetworkAccessManager *gNetworkAccessManager;
+
 ClicksCounter::ClicksCounter(QObject *parent) :
     QObject(parent)
 {
+    qMemSet( m_clicksQueue, 0, sizeof(m_clicksQueue) );
+}
+
+void ClicksCounter::update( int page, int index )
+{
+    QSqlDatabase db = QSqlDatabase::database( IviasClientDBConnection );
+    const int id = page * cAdsPerPage + index;
+
+    if( gNetworkAccessManager->networkAccessible() == QNetworkAccessManager::Accessible && db.isOpen() )
+    {
+        // update DB
+        const QString1Literal command("UPDATE stats SET clicks = clicks+1 WHERE cid = %1 AND adId = %2");
+        QSqlQuery query( QString(command).arg(/*TODO: client id*/).arg(id), db );
+
+        if(!query.exec()) {
+            // error. Add click to queue
+            m_clicksQueue[id]++;
+        }
+    }
+    else
+    {
+        // update queue
+        m_clicksQueue[id]++;
+    }
+}
+
+void ClicksCounter::flushQueue()
+{
+    QSqlDatabase db = QSqlDatabase::database( IviasClientDBConnection );
+
+    if( gNetworkAccessManager->networkAccessible() == QNetworkAccessManager::Accessible && db.isOpen() )
+    {
+        const QString1Literal command("UPDATE stats SET clicks = clicks+%1 WHERE cid = %2 AND adId = %3");
+
+        for( int i = 0; i < cTotalNumberOfAds; i++ )
+        {
+            if( m_clicksQueue[i] != 0 ) {
+                QSqlQuery query( QString(command).arg(m_clicksQueue[i]).arg(/*TODO: client id*/).arg(id), db );
+
+                if( query.exec() ) {
+                    m_clicksQueue[i] = 0;
+                }
+            }
+        }
+    }
 }
--- clickscounter.h
+++ clickscounter.h
@@ -1,18 +1,25 @@
 #ifndef CLICKSCOUNTER_H
 #define CLICKSCOUNTER_H
 
-#include <QObject>
+#include <QtCore/QObject>
+#include <QtCore/QPair>
+
+#include "constraints.h"
 
 class ClicksCounter : public QObject
 {
     Q_OBJECT
 public:
     explicit ClicksCounter(QObject *parent = 0);
+
+    Q_INVOKABLE void update( int page, int index );
+
+    void flushQueue();
     
-signals:
-    
-public slots:
-    
+    //TODO: Save queue on exit, if there are unupdated values
+
+private:
+    int m_clicksQueue[cTotalNumberOfAds];
 };
 
 #endif // CLICKSCOUNTER_H
